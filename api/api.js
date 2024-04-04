6303f8aadaa0cbfd5eee4f8c3acf15e31e1f4e1b
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-04-04 17:15:59 -0500

    finish hw

--- api/api.js
+++ api/api.js
@@ -1,16 +1,33 @@
 const pokeInfo = async () => {
-    // grabs the value the user puts into the text box after
-    // clicking the button
-    var userInput = document.getElementById("input").value;
-    console.log(userInput)
-    // calls the pokeAPI with the user input as the pokemon name
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`);
-    // jsonifys the response
-    const data = await response.json();
-    // grabs the pokemons front sprite
-    console.log('image', data["sprites"]["front_default"])
-    // grabs the pokemons name
-    console.log('name', data["name"]);
-    // Clears the text box without reloading the page
-    var userInput = document.getElementById("input").value = '';
+    // Try catch block since it calls to an API
+    try{
+        // grabs the value the user puts into the text box after
+        // clicking the button
+        var userInput = (document.getElementById("input").value).toLowerCase();
+    
+        // calls the pokeAPI with the user input as the pokemon name
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`);
+        // jsonifys the response
+        const data = await response.json();
+    
+        // grabs the pokemons front sprite
+        const sprite = data["sprites"]["front_default"]
+        // grabs the pokemons name
+        const name = data["name"]
+    
+        // creates new div for the pokemon name output
+        const pokeName = document.createElement("div");
+        // puts the information in the new div
+        pokeName.innerHTML = `This is the Pokemons name: ${name}`;
+        // appends the info and puts it on the webpage
+        document.body.appendChild(pokeName);
+    
+        // grabs the id in the dom and assigns the src to the sprite api location
+        document.getElementById('pokeImage').src = sprite;
+    
+        // Clears the text box without reloading the page
+        var userInput = document.getElementById("input").value = '';
+    }catch {
+        alert('That\'s not a Pokemon!')
+    }
 }
